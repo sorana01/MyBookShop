afd8b638f07f3b4bd6e39f45ad602555e326c286
stefidone <stefandone3@gmail.com>
2022-05-21 00:47:15 +0300

    SEF-44 Added functionality to Add Book button

--- src/main/java/com/example/book_shop/controllers/ManagerController.java
+++ src/main/java/com/example/book_shop/controllers/ManagerController.java
@@ -1,9 +1,22 @@
 package com.example.book_shop.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class ManagerController {
-    public void addBook(ActionEvent actionEvent) {
+    public void addBook(ActionEvent actionEvent) throws IOException {
+        Parent root1 = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("add_books.fxml")));
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("Add Books");
+        window.setScene(new Scene(root1, 600, 460));
+        window.show();
     }
 
     public void viewOrders(ActionEvent actionEvent) {
