dd7ece97e88efc21b4294d16d5ccbe6aaaf3f230
Sorana <doris.haiduc@student.upt.ro>
2022-05-21 00:33:30 +0300

    SEF-71 Created Courier Window

---
+++ src/main/java/com/example/book_shop/controllers/CourierSeeOrdersController.java
@@ -0,0 +1,8 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class CourierSeeOrdersController {
+    public void deliverOrder(ActionEvent actionEvent) {
+    }
+}
