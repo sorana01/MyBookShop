ece254be6ec3d490b4e1c907fa6be5e932e16f99
stefidone <stefandone3@gmail.com>
2022-05-21 00:48:36 +0300

    SEF-44 Added functionality to Back button

--- src/main/java/com/example/book_shop/controllers/AddBooksController.java
+++ src/main/java/com/example/book_shop/controllers/AddBooksController.java
@@ -1,9 +1,21 @@
 package com.example.book_shop.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AddBooksController {
-    public void back_button(ActionEvent actionEvent) {
+    public void back_button(ActionEvent actionEvent) throws IOException {
+        Parent adminTripPageInterface = FXMLLoader.load(getClass().getClassLoader().getResource("manager_page.fxml"));
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("Book Shop");
+        window.setScene(new Scene(adminTripPageInterface, 600, 460));
+        window.show();
     }
 
     public void add_book(ActionEvent actionEvent) {
