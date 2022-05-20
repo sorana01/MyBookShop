063c6f4c8db7970dea79cb4c45990777227f5c83
stefidone <stefandone3@gmail.com>
2022-05-21 00:41:36 +0300

    SEF-44 Created Add Books Window

---
+++ src/main/java/com/example/book_shop/controllers/AddBooksController.java
@@ -0,0 +1,11 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class AddBooksController {
+    public void back_button(ActionEvent actionEvent) {
+    }
+
+    public void add_book(ActionEvent actionEvent) {
+    }
+}
