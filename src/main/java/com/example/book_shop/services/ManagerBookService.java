348f901fb56d8be9d59a0d0f953119f57b86d073
stefidone <stefandone3@gmail.com>
2022-05-21 00:55:07 +0300

    SEF-37 Created Database for Storing Books

---
+++ src/main/java/com/example/book_shop/services/ManagerBookService.java
@@ -0,0 +1,35 @@
+package com.example.book_shop.services;
+
+import com.example.book_shop.model.Book;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+
+public class ManagerBookService {
+    private static List<Book> book_database;
+    private static final Path BOOKS_PATH = FileSystemService.getPathToFile("books.json");
+
+    public static List<Book> getBooks() {
+        return book_database;
+    }
+
+
+    public static void loadBooksFromFile() throws IOException {
+        if (!Files.exists(BOOKS_PATH)) {
+            FileUtils.copyURLToFile(Objects.requireNonNull(UserService.class.getClassLoader().getResource("books.json")), BOOKS_PATH.toFile());
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        book_database = objectMapper.readValue(BOOKS_PATH.toFile(), new TypeReference<>() {
+        });
+    }
+
+
+
+}
