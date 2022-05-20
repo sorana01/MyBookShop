fac50b5b91d2e39372b17ac5a98d0dd8812fb8bf
stefidone <stefandone3@gmail.com>
2022-05-21 01:25:06 +0300

    SEF-38 Created Edit Books Window

---
+++ src/main/java/com/example/book_shop/controllers/EditBooksController.java
@@ -0,0 +1,11 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class EditBooksController {
+    public void back_button(ActionEvent actionEvent) {
+    }
+
+    public void edit_book(ActionEvent actionEvent) {
+    }
+}
