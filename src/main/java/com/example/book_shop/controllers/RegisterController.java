f1f5a4a0f7305eeefd93cb3f1ce16494c7552276
Sorana <doris.haiduc@student.upt.ro>
2022-05-20 23:12:59 +0300

    SEF-33 Added functionality to Back to Login button

--- src/main/java/com/example/book_shop/controllers/RegisterController.java
+++ src/main/java/com/example/book_shop/controllers/RegisterController.java
@@ -2,12 +2,18 @@ package com.example.book_shop.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class RegisterController {
     @FXML
@@ -41,6 +47,10 @@ public class RegisterController {
     }
 
     public void goBackToLogin(ActionEvent login) throws IOException {
-
+        Parent backToLogin = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
+        Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();;
+        window.setTitle("Login");
+        window.setScene(new Scene(backToLogin, 600, 400));
+        window.show();
     }
 }
