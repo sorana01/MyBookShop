4ca600016864df8e91494de5b58ee62849f0faed
Sorana <doris.haiduc@student.upt.ro>
2022-05-21 00:31:04 +0300

    SEF-72 Created Manager Window

---
+++ src/main/java/com/example/book_shop/controllers/ManagerController.java
@@ -0,0 +1,17 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class ManagerController {
+    public void addBook(ActionEvent actionEvent) {
+    }
+
+    public void viewOrders(ActionEvent actionEvent) {
+    }
+
+    public void deleteBook(ActionEvent actionEvent) {
+    }
+
+    public void editBook(ActionEvent actionEvent) {
+    }
+}
