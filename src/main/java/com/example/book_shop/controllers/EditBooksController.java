7e38b39e691c1b0f67a7d18c54619d6abe9217a8
stefidone <stefandone3@gmail.com>
2022-05-21 10:04:56 +0300

    SEF-48 Created back button to return to Manager Page

--- src/main/java/com/example/book_shop/controllers/EditBooksController.java
+++ src/main/java/com/example/book_shop/controllers/EditBooksController.java
@@ -6,9 +6,16 @@ import com.example.book_shop.exceptions.NothingToEditException;
 import com.example.book_shop.services.ManagerBookService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class EditBooksController {
 
@@ -34,7 +41,12 @@ public class EditBooksController {
         category_new.getItems().addAll("Romance", "Horror", "Thriller", "Science Fiction", "Others");
     }
 
-    public void back_button(ActionEvent actionEvent) {
+    public void back_button(ActionEvent actionEvent) throws IOException {
+        Parent adminTripPageInterface = FXMLLoader.load(getClass().getClassLoader().getResource("manager_page.fxml"));
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("Book Shop");
+        window.setScene(new Scene(adminTripPageInterface, 600, 460));
+        window.show();
     }
 
     public void edit_book(ActionEvent actionEvent) {
