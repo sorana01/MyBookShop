49565cba614e7bb6b61b19d538ecfd55a7fa4873
timiirimia <timoteiirimia9@gmail.com>
2022-05-21 10:34:18 +0300

    SEF-40 Retrieved products from database and showed them on screen

--- src/main/java/com/example/book_shop/controllers/ClientController.java
+++ src/main/java/com/example/book_shop/controllers/ClientController.java
@@ -1,8 +1,47 @@
 package com.example.book_shop.controllers;
 
+import com.example.book_shop.model.Book;
+import com.example.book_shop.services.ManagerBookService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+import java.util.List;
 
 public class ClientController {
+    private static List<Book> book_database = ManagerBookService.getBooks();
+
+    @FXML
+    public TableView<Book> books_view = new TableView<>();
+
+    @FXML
+    public TableColumn<Book, String> title_col;
+    @FXML
+    public TableColumn<Book, String> author_col;
+    @FXML
+    public TableColumn<Book, String> price_col;
+    @FXML
+    public TableColumn<Book, String> category_col;
+
+    @FXML
+    public void initialize() throws IOException {
+        ObservableList<Book> items = FXCollections.observableArrayList();
+        title_col.setCellValueFactory(new PropertyValueFactory<>("Title"));
+        author_col.setCellValueFactory(new PropertyValueFactory<>("Author"));
+        price_col.setCellValueFactory(new PropertyValueFactory<>("Price"));
+        category_col.setCellValueFactory(new PropertyValueFactory<>("Category"));
+
+        for (Book book : book_database) {
+            items.add(book);
+            books_view.setItems(items);
+        }
+    }
+
     public void addToCart(ActionEvent actionEvent) {
     }
 
