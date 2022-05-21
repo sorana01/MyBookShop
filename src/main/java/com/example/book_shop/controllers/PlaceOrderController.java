3a7d4732b46caf39f35c05d7f1116f405afd5068
timiirimia <timoteiirimia9@gmail.com>
2022-05-21 11:53:37 +0300

    SEF-78 Created Place Order Window

---
+++ src/main/java/com/example/book_shop/controllers/PlaceOrderController.java
@@ -0,0 +1,11 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class PlaceOrderController {
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    public void finishOrder(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/book_shop/controllers/SeeCartController.java
+++ src/main/java/com/example/book_shop/controllers/SeeCartController.java
@@ -1,9 +1,21 @@
 package com.example.book_shop.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SeeCartController {
-    public void placeOrder(ActionEvent actionEvent) {
+    public void placeOrder(ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("place_order.fxml"));
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("Finish Purchase");
+        window.setScene(new Scene(parent, 600, 460));
+        window.show();
     }
 
     public void backButton(ActionEvent actionEvent) {
