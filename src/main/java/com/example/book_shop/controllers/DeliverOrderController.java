94b7125f2e8661a21192ce5eaaddba1fc38ec734
Sorana <doris.haiduc@student.upt.ro>
2022-05-21 21:23:28 +0300

    SEF-83 Created back button to return to Courier Page

--- src/main/java/com/example/book_shop/controllers/DeliverOrderController.java
+++ src/main/java/com/example/book_shop/controllers/DeliverOrderController.java
@@ -8,9 +8,17 @@ import com.example.book_shop.services.CourierBookService;
 import com.example.book_shop.services.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class DeliverOrderController {
     @FXML
@@ -40,6 +48,11 @@ public class DeliverOrderController {
         }
     }
 
-    public void backButton(ActionEvent actionEvent) {
+    public void backButton(ActionEvent actionEvent) throws IOException {
+        Parent root1 = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("courier_page.fxml")));
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("Book Shop");
+        window.setScene(new Scene(root1, 600, 460));
+        window.show();
     }
 }
