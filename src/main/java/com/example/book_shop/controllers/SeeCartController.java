355051185b459d55c8703e104de342500a012ded
timiirimia <timoteiirimia9@gmail.com>
2022-05-21 10:53:14 +0300

    SEF-75 Created Window for See the Products in the Cart

---
+++ src/main/java/com/example/book_shop/controllers/SeeCartController.java
@@ -0,0 +1,11 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class SeeCartController {
+    public void placeOrder(ActionEvent actionEvent) {
+    }
+
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
