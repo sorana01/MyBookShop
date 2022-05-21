9ee1905f23d7a4acc5c43d8028475c314be94ed1
Sorana <doris.haiduc@student.upt.ro>
2022-05-21 16:11:31 +0300

    SEF-82 Created Deliver Order Window

--- src/main/java/com/example/book_shop/controllers/CourierSeeOrdersController.java
+++ src/main/java/com/example/book_shop/controllers/CourierSeeOrdersController.java
@@ -1,8 +1,21 @@
 package com.example.book_shop.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class CourierSeeOrdersController {
-    public void deliverOrder(ActionEvent actionEvent) {
+    public void deliverOrder(ActionEvent actionEvent) throws IOException {
+        Parent root1 = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("deliver_order.fxml")));
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("View Orders");
+        window.setScene(new Scene(root1, 600, 460));
+        window.show();
     }
 }
---
+++ src/main/java/com/example/book_shop/controllers/DeliverOrderController.java
@@ -0,0 +1,12 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class DeliverOrderController {
+
+    public void saveButton(ActionEvent actionEvent) {
+    }
+
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
