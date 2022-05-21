f45b65b40c9f5cf3285b393c423e0f342dd318cb
stefidone <stefandone3@gmail.com>
2022-05-21 15:34:23 +0300

    SEF-53 Created Accept Or Reject Window

---
+++ src/main/java/com/example/book_shop/controllers/AcceptOrRejectController.java
@@ -0,0 +1,11 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class AcceptOrRejectController {
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    public void saveButton(ActionEvent actionEvent) {
+    }
+}
