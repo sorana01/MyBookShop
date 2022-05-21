3068a31a769a8c7c232646d4fb31fc9460db8b75
timiirimia <timoteiirimia9@gmail.com>
2022-05-21 12:18:16 +0300

    SEF-59 Managed to successfully add a product to the shopping cart

--- src/main/java/com/example/book_shop/controllers/AddToCartController.java
+++ src/main/java/com/example/book_shop/controllers/AddToCartController.java
@@ -1,19 +1,50 @@
 package com.example.book_shop.controllers;
 
+import com.example.book_shop.exceptions.BookDoesntExistException;
+import com.example.book_shop.exceptions.EmptyTextFieldsException;
+import com.example.book_shop.exceptions.InvalidQuantityException;
+import com.example.book_shop.exceptions.NotAllCharactersAreDigitsException;
+import com.example.book_shop.services.ClientBookService;
+import com.example.book_shop.services.UserService;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AddToCartController {
+    @FXML
+    private TextField title;
+    @FXML
+    private TextField author;
+    @FXML
+    private TextField quantity;
+    @FXML
+    private Label message;
+
+
     public void backButton(ActionEvent actionEvent) {
     }
 
     public void addToCart(ActionEvent actionEvent) {
+        try {
+            ClientBookService.addABookToCart(title.getText(), author.getText(), quantity.getText());
+            message.setText("Book added to Cart!");
+        } catch (BookDoesntExistException e) {
+            message.setText(e.getMessage());
+        } catch(EmptyTextFieldsException e) {
+            message.setText(e.getMessage());
+        } catch(InvalidQuantityException e) {
+            message.setText(e.getMessage());
+        } catch (NotAllCharactersAreDigitsException e) {
+            message.setText(e.getMessage());
+        }
     }
 
     public void seeCart(ActionEvent actionEvent) throws IOException {
--- src/main/java/com/example/book_shop/services/ClientBookService.java
+++ src/main/java/com/example/book_shop/services/ClientBookService.java
@@ -1,6 +1,6 @@
 package com.example.book_shop.services;
 
-import com.example.book_shop.exceptions.CouldNotWriteBooksException;
+import com.example.book_shop.exceptions.*;
 import com.example.book_shop.model.Book;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,9 +13,10 @@ import java.util.List;
 import java.util.Objects;
 
 public class ClientBookService {
+    private static List<Book> book_database = ManagerBookService.getBooks();
     private static List<Book> shopping_cart_database;
     private static final Path SHOPPING_CART_PATH = FileSystemService.getPathToFile("shopping_cart.json");
-
+    private static Book my_book;
 
 
 
@@ -27,6 +28,41 @@ public class ClientBookService {
         ObjectMapper objectMapper = new ObjectMapper();
         shopping_cart_database = objectMapper.readValue(SHOPPING_CART_PATH.toFile(), new TypeReference<>() {
         });
+    }
+
+    public static void addABookToCart(String title, String author, String quantity) throws EmptyTextFieldsException, InvalidQuantityException, BookDoesntExistException, NotAllCharactersAreDigitsException {
+        checkEmptyTextFields(title, author, quantity);
+        UserService.checkAllDigitsEntered(quantity);
+        checkIfBookAvailable(title, author, quantity);
+
+        //add the product to a temporary database (shopping cart)
+        shopping_cart_database.add(new Book(my_book.getTitle(), my_book.getPrice(), my_book.getCategory(), quantity, my_book.getAuthor()));
+        persistShoppingCart();
+    }
+
+    private static void checkEmptyTextFields(String title, String author, String quantity) throws EmptyTextFieldsException {
+
+        if( Objects.equals(title,"") || Objects.equals(author, "") || Objects.equals(quantity,""))
+            throw new EmptyTextFieldsException();
+
+    }
+
+    private static void checkIfBookAvailable(String title, String author, String quantity) throws BookDoesntExistException, InvalidQuantityException {
+        int book_exists = 0;
+
+        for (Book book : book_database) {
+            if (Objects.equals(author, book.getAuthor()) && Objects.equals(title, book.getTitle())) {
+                book_exists = 1;
+                if (Integer.parseInt(quantity) < 0 || (Integer.parseInt(quantity) > Integer.parseInt(book.getQuantity()))) {
+                    throw new InvalidQuantityException();
+                }
+                my_book = book;
+                break;
+            }
+        }
+
+        if (book_exists == 0)
+            throw new BookDoesntExistException();
     }
 
     private static void persistShoppingCart() {
