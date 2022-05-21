d3e41a2fc3d4b848bb741207918a359116917720
timiirimia <timoteiirimia9@gmail.com>
2022-05-21 10:37:39 +0300

    SEF-58 Created Window to Add a Product or more to the Cart

---
+++ src/main/java/com/example/book_shop/controllers/AddToCartController.java
@@ -0,0 +1,14 @@
+package com.example.book_shop.controllers;
+
+import javafx.event.ActionEvent;
+
+public class AddToCartController {
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    public void addToCart(ActionEvent actionEvent) {
+    }
+
+    public void seeCart(ActionEvent actionEvent) {
+    }
+}
