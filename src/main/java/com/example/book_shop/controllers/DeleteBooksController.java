a569cb4d2e5601199986b825e49a0856aca8e492
stefidone <stefandone3@gmail.com>
2022-05-21 10:23:11 +0300

    SEF-51 Back button to return to Manager Page

--- src/main/java/com/example/book_shop/controllers/DeleteBooksController.java
+++ src/main/java/com/example/book_shop/controllers/DeleteBooksController.java
@@ -5,8 +5,13 @@ import com.example.book_shop.exceptions.EmptyTextFieldsException;
 import com.example.book_shop.services.ManagerBookService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -31,6 +36,10 @@ public class DeleteBooksController {
     }
 
     public void back_button(ActionEvent actionEvent) throws IOException {
-
+        Parent adminTripPageInterface = FXMLLoader.load(getClass().getClassLoader().getResource("manager_page.fxml"));
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("Book Shop");
+        window.setScene(new Scene(adminTripPageInterface, 600, 460));
+        window.show();
     }
 }
